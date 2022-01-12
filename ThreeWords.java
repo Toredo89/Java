1c951d18a489be64c6bb996d8372e425e3731dec
Toredo89 <87312094+Toredo89@users.noreply.github.com>
2022-01-12 21:08:51 +0500

    Add files via upload

---
+++ ThreeWords.java
@@ -0,0 +1,7 @@
+class ThreeWords {
+	public static void main(String[] args) {
+		System.out.println("Orange");
+		System.out.println("Banana");
+		System.out.println("Apple");
+		}
+	}
---
+++ checkSumSign.java
@@ -0,0 +1,15 @@
+class checkSumSign {
+	public static void main(String[] args) {
+		int first = 5;
+		int second = 2;
+
+		if (first > 5) {
+			System.out.println("Сумма положительная");
+		}
+		if (second < 5) {
+			System.out.println("Сумма отрицательная");
+		}
+		
+
+	}
+}
